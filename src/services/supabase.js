bf7e94810422a3f7741335453428f7286bfc44a9
APEXLOGIC2025 <apexlogic2025@gmail.com>
2026-05-25 21:15:07 +0530

    Update supabase.js

--- src/services/supabase.js
+++ src/services/supabase.js
@@ -1,5 +1,20 @@
-import {createClient} from '@supabase/supabase-js'
-export default createClient(
- import.meta.env.VITE_SUPABASE_URL,
- import.meta.env.VITE_SUPABASE_ANON_KEY
-)
+import { createClient } from "@supabase/supabase-js";
+
+const supabaseUrl =
+  import.meta.env.VITE_SUPABASE_URL;
+
+const supabaseAnonKey =
+  import.meta.env.VITE_SUPABASE_ANON_KEY;
+
+console.log(
+"SUPABASE URL:",
+supabaseUrl
+);
+
+const supabase =
+createClient(
+  supabaseUrl,
+  supabaseAnonKey
+);
+
+export default supabase;
